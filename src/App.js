f47d71dd91bc4d0fa884ce2650509fa5f8dc5ec6
unknown <saied2421998@gmail.com>
2022-03-31 11:53:18 +0200

    initialize components  & Routes

--- src/App.js
+++ src/App.js
@@ -1,2 +1,15 @@
-const App = () => <h1>React App</h1>;
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Books from './components/Books/Books';
+import Categories from './components/Categories/Categories';
+
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="categories" element={<Categories />} />
+    </Routes>
+  </>
+);
 export default App;
