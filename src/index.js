85715cb448626f4b0db20bb888b81f99ad245592
unknown <saied2421998@gmail.com>
2022-04-04 15:37:22 +0200

    fix errors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import store from './Redux/configureStore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Provider>
+      <Provider store={store}>
         <App />
       </Provider>
     </Router>
